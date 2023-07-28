b991b5602cdf4c45b552ed7f5efb3ca583b983fa
alhad-tm <alhadchikkutm@gmail.com>
2023-07-28 10:10:44 +0530

    about

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <About/>
     
     </div>
   );
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import css from "./About.module.css"
+import Who from "../../assets/whovr.svg"
+
+const About = () => {
+  return (
+    <div className={css.container}>
+        <div className={css.left}>
+
+            <div className={css.leftone}>
+                <span>Who We Are !</span>
+                <span>With a rich legacy of 17 years in the construction industry, The Green Castle 
+                    stands tall as a distinguished provider of exceptional construction services.
+                     Our unwavering commitment to excellence and a steadfast dedication to our clients' 
+                     visions have earned us a reputable position in the market. <br />
+
+      At The Green Castle, we take pride in offering a comprehensive range of services that encompass design,
+ planning, and execution. Whether you aspire to bring your dream home to life, embark on a commercial venture,
+  or undertake a renovation project, we possess the expertise and experience
+   to transform your dreams into reality.</span>
+            </div>
+
+        </div>
+
+
+        <div className={css.right}>
+            <img src={Who} alt="" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
