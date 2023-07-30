c2680ed4aee1a15ebc95c492715898447959cb2e
alhad-tm <alhadchikkutm@gmail.com>
2023-07-30 20:37:32 +0530

    final layout

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,10 @@ import Logo1 from "../../assets/Green castel logo.svg"
 import Twitter from "../../assets/twitter logo.svg"
 import Fb from "../../assets/Facebook logo.svg"
 import Insta from "../../assets/instagram logo.svg"
+import { Link } from 'react-scroll'
 
 const Footer = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
   return (
     <div className={css.container}>
         <div className={css.div1}>
@@ -14,11 +16,12 @@ const Footer = () => {
 
 
         <ul className={css.div2list}>
-           <li className={css}>About</li> 
-           <li>Services</li>
-           <li>Contact</li>
-           <li>Privacy Policy</li>
-           <li>Terms & Conditions</li> 
+        <li><Link to="about" spy={true} smooth={true}>About</Link></li>
+                <li><Link to="services" spy={true} smooth={true}>Services</Link></li>
+                <li><Link to="contact" spy={true} smooth={true}>Contact</Link></li>
+        {mobile?   <li>Privacy</li> :  <li>Privacy Policy</li>}
+        {mobile?   <li>T&C</li> :  <li>Terms and Conditions</li>}
+        
         </ul>
 
 
